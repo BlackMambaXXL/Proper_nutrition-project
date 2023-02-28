258b49f2fed633698f138026aaad62e0547486a7
Niyaz <khairutdinov96@gmail.com>
2023-02-28 16:02:44 +0300

    refactored classes and added tabs switching animation

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
-const tabsMenuWrapper = document.querySelector('.tabheader__items'),
+window.addEventListener('DOMContentLoaded', () => {
+    const tabsMenuWrapper = document.querySelector('.tabheader__items'),
       tabsMenuBody = document.querySelectorAll('.tabheader__item'),
       tabContent = document.querySelectorAll('.tabcontent');
 
-tabsMenuWrapper.addEventListener('click', (event) => {
-    event.preventDefault();
-    if (event.target && event.target.matches('div.tabheader__item')) {
-        tabsMenuBody.forEach((element, index) => {
-            element.classList.remove('tabheader__item_active');
-            if (event.target == element) {
-                tabContent.forEach(element => {
-                    element.classList.add('tabcontent__item_hiden');
-                });
-                tabContent[index].classList.remove('tabcontent__item_hiden');
-            }
-        });
-        event.target.classList.add('tabheader__item_active');
-    }
+    tabsMenuWrapper.addEventListener('click', (event) => {
+        event.preventDefault();
+        if (event.target && event.target.matches('div.tabheader__item')) {
+            tabsMenuBody.forEach((element, index) => {
+                element.classList.remove('tabheader__item_active');
+                if (event.target == element) {
+                    tabContent.forEach(element => {
+                        element.classList.add('hide');
+                    });
+                    tabContent[index].classList.remove('hide');
+                    tabContent[index].classList.add('fade');
+                }
+            });
+            event.target.classList.add('tabheader__item_active');
+        }
+    });
 });
 
       
