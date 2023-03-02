0c0f5d7ef1832e51a713c59655aec8b28dd29100
Niyaz <khairutdinov96@gmail.com>
2023-03-02 15:58:07 +0300

    improved the timer

--- js/script.js
+++ js/script.js
@@ -21,13 +21,22 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
     // Timer
-    const deadLine = '2023-03-20';
+    const deadLine = '2023-06-01';
     function getTimeRemaining(endTime) {
-        const t = Date.parse(endTime) - Date.parse(new Date()),
-              days = Math.floor(t / (1000 * 60 * 60 * 24)),
-              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-              minutes = Math.floor((t / 1000 / 60) % 60),
-              seconds = Math.floor((t / 1000) % 60);
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endTime) - Date.parse(new Date());
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor(((t / (1000 * 60 * 60) % 24)) - 3),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        }
+
         return {
             'total': t,
             'days': days,
@@ -38,6 +47,14 @@ window.addEventListener('DOMContentLoaded', () => {
         
     }
     
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
     function setTimer(selector, endTime) {
         const timer = document.querySelector(selector),
               days = timer.querySelector('#days'),
@@ -45,14 +62,14 @@ window.addEventListener('DOMContentLoaded', () => {
               minutes = timer.querySelector('#minutes'),
               seconds = timer.querySelector('#seconds'),
               timeInterval = setInterval(updateTimer, 1000);
-
+        updateTimer();
         function updateTimer() {
             const t = getTimeRemaining(endTime);
 
-            days.innerHTML = t.days;
-            hours.innerHTML = t.hours;
-            minutes.innerHTML = t.minutes;
-            seconds.innerHTML = t.seconds;
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
 
             if (t.total < 0) {
                 clearInterval(timeInterval);
